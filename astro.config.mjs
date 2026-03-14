8ff0a1b13a6e665c0d8d7de038cf90a9db47b97c
AlexzzDev <alexzdevcontacto@gmail.com>
2026-03-14 17:41:46 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Ejemplo: https://AlexzDev.github.io
-  site: 'https://alexzzdev.github.io',
-  base: '/NOMBRE_REPO', 
+  site: 'https://alexzzdev1.github.io',
+  base: '/', // Asegúrate de que esto sea solo una barra o no esté
   integrations: [tailwind()],
 });
-
